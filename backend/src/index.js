4e5de6ad36eb53ba2c1bf1011f33969d609c16a6
desireemenezes <deemenezes@hotmail.com>
2020-08-15 14:15:11 -0300

    create task + routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const server = express(); // carrega server
+server.use(express.json())
 
-server.get('/teste', (req, res) => {
-    res.send('Welcome server');
-});
+const TaskRoutes = require('./routes/TaskRoutes');
+server.use('/task', TaskRoutes);
 
 server.listen(7000, () => {
     console.log('Api online')
---
+++ backend/src/routes/TaskRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const TaskController = require('../controller/TaskController')
+
+router.post('/', TaskController.create);
+
+module.exports = router;

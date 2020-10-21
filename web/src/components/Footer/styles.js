241283f92fcf96f3507f5b9ed50f56b067b3b4a3
desireemenezes <deemenezes@hotmail.com>
2020-10-20 22:31:51 -0300

    add front

---
+++ web/src/components/Footer/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+    width: 100%;
+    height: 50px;
+    background-color: #20295f;
+    border-top: 5px solid #ee6b26;
+
+    position: fixed;
+    bottom: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    span {
+        color: #fff;
+        
+    }
+`
